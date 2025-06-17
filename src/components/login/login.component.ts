0d762bdf4b90690b7428d9ea0723e47522d96b1a
shiiim5 <shimaaaglan27@gmail.com>
2025-06-17 23:24:17 +0300

    init and add login component

---
+++ src/components/login/login.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  imports:[ReactiveFormsModule,CommonModule]
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  //showPassword: boolean = false;
+
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email,Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+      password: ['', [Validators.required ,Validators.minLength(6), Validators.pattern(/^(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*\d).+$/)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Login data:', this.loginForm.value);
+      // Call your login service here
+    }
+  }
+}
