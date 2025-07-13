580fc2072e775dabd4cf566f5a8bed289abbbfab
Ahmed Hatem <ahmedhatem722001@gmail.com>
2025-07-13 06:59:35 +0300

    add validation to helper add service and some desgin

--- src/components/helperaddservice/helperaddservice.component.ts
+++ src/components/helperaddservice/helperaddservice.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { HelperservicesService } from '../../core/services/helperservices.service';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -7,6 +7,7 @@ import { GetloggineduserDataService } from 'core/services/getloggineduser-data.s
 import { ServiceCategory, ServiceCategoryService } from '@services/service-category.service';
 import { ToastrService } from 'ngx-toastr';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-helperaddservice',
@@ -31,15 +32,44 @@ export class HelperaddserviceComponent implements OnInit {
    Allcategories!:ServiceCategory[]
     private readonly _ServiceCategoryService=inject(ServiceCategoryService)
   addHelperService: FormGroup = new FormGroup({
-    description: new FormControl(''),
-    pricePerHour: new FormControl(''),
+    description: new FormControl('',[Validators.required,Validators.minLength(10)]),
+    pricePerHour: new FormControl('',[Validators.required]),
     availableDateFrom: new FormControl(''),
     availableDateTo: new FormControl(''),
     helperId: new FormControl(null),
    serviceCategoryId: new FormControl<number | null>(null),
 
     status: new FormControl('0'),
-  });
+  },this.validateTimes);
+
+   validateTimes(g: AbstractControl) {
+    const datenow = new Date();
+    const fromValue = g.get('availableDateFrom')?.value;
+    const toValue = g.get('availableDateTo')?.value;
+
+   
+    if (!fromValue || !toValue) {
+        return { required: true };
+    }
+
+ 
+    const fromDate = new Date(fromValue);
+    const toDate = new Date(toValue);
+
+    if (fromDate <= datenow) {
+        return { fromBeforeNow: true };
+    }
+
+    if (toDate <= datenow) {
+        return { toBeforeNow: true };
+    }
+
+    if (toDate < fromDate) {
+        return { toBeforeFrom: true };
+    }
+
+    return null;
+}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
@@ -92,46 +122,53 @@ export class HelperaddserviceComponent implements OnInit {
     }
   }
 
-  test(): void {
-    this.isloading = true;
-    const token = localStorage.getItem('token');
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`,
-    });
-
-    const data = {
-      id: this.serviceId, 
-      ...this.addHelperService.value
-    };
-
-    if (this.isEditMode && this.serviceId) {
-      this._helperservicesService.updateService(this.serviceId, data, headers).subscribe({
-        next: (res) => {
-          this.isloading = false;
-           this._toster.success('service updated successfully')
-          this.router.navigate(['/provider/services']);
-          console.log('Service updated:', res);
-        },
-        error: (err) => {
-          this.isloading = false;
-             this._toster.error(' cannot  update this service ')
-          console.error('Update error:', err);
-        }
-      });
-    } else {
-      this._helperservicesService.addhelperservice(data, headers).subscribe({
-        next: (res) => {
-          this.isloading = false;
-          this._toster.success('service Added successfully')
-          this.router.navigate(['/provider/services']);
-          console.log('Service added:', res);
-        },
-        error: (err) => {
-          this.isloading = false;
-           this._toster.error(' cannot add this service ')
-          console.error('Add error:', err);
-        }
-      });
-    }
+test(): void {
+ 
+  if (this.addHelperService.invalid) {
+    this.addHelperService.markAllAsTouched(); 
+    this._toster.error('Please correct the errors in the form before saving.');
+    return;
   }
+
+  this.isloading = true;
+
+  const token = localStorage.getItem('token');
+  const headers = new HttpHeaders({
+    Authorization: `Bearer ${token}`,
+  });
+
+  const data = {
+    id: this.serviceId,
+    ...this.addHelperService.value
+  };
+
+ 
+  if (this.isEditMode && this.serviceId) {
+    this._helperservicesService.updateService(this.serviceId, data, headers).subscribe({
+      next: (res) => {
+        this.isloading = false;
+        this._toster.success('Service updated successfully');
+        this.router.navigate(['/provider/services']);
+      },
+      error: (err) => {
+        this.isloading = false;
+        this._toster.error('Failed to update service');
+        console.error('Update error:', err);
+      }
+    });
+  } else {
+    this._helperservicesService.addhelperservice(data, headers).subscribe({
+      next: (res) => {
+        this.isloading = false;
+        this._toster.success('Service added successfully');
+        this.router.navigate(['/provider/services']);
+      },
+      error: (err) => {
+        this.isloading = false;
+        this._toster.error('Failed to add service');
+        console.error('Add error:', err);
+      }
+    });
+  }
+}
 }
